e4de2505253ce061e5ecc6f5e37d4bdb0526f7f7
idith almog <almog.idith@gmail.com>
2020-04-30 18:44:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,11 @@ def summon_captain_planet(array)
 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  if calls.detect {|call| call.length > 4} true
+  else
+    false
+  end
 end
 
 def find_the_cheese# code an argument here
